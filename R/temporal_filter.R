3fd7a7e500602b12a88167f287ac2dcceb6668b9
Matt Denny <matthewjdenny@gmail.com>
2016-06-17 12:07:23 -0400

    function to filter dfm on a threshold number of years terms must appear in

---
+++ R/temporal_filter.R
@@ -0,0 +1,63 @@
+#' @title Remove terms that only appear in a less than a specified proportion of
+#' years.
+#' @description Removes terms appearing in less than a specifiec percentage of
+#' years in the data.
+#'
+#' @param dfm_object A quanteda dfm() object.
+#' @param years A numeric vector giving the year for each document
+#' @param proportion_threshold proportion of years a term must be included in
+#' to be included in the dfm.
+#' @return A reduced dfm.
+#' @export
+temporal_filter <- function(dfm_object,
+                            years,
+                            proportion_threshold = 0.5){
+
+    # create a temporary dfm object
+    temp_dfm <- dfm_object
+
+    #set word counts to 1
+    temp_dfm@x <- rep(1,length(temp_dfm@x))
+
+    temp_dfm <- as.matrix(temp_dfm)
+
+    # now create a matrix with one row for each year
+    unique_years <- unique(years)
+    num_years <- length(unique_years)
+    year_counts <- matrix(0,nrow = num_years, ncol = ncol(temp_dfm))
+
+    # determine the number of years a term must appear in to be kept
+    threshold <- ceiling(proportion_threshold * num_years)
+
+    # loop over years
+    for(i in 1:num_years) {
+        inds <- which(years == unique_years[i])
+        temp <- temp_dfm[inds,]
+        counts <- colSums(temp)
+
+        # figure out which counts are greater than 0 and set them to 1
+        gz <- which(counts > 0 )
+        if (length(gz) > 0) {
+            counts[gz] <- 1
+        }
+
+        #store
+        year_counts[i,] <- counts
+    }
+
+    # get column sums
+    year_totals <- colSums(year_counts)
+
+    # determine which column
+    remove <- as.numeric(which(year_totals < threshold))
+    cat("Removing",length(remove),"of",ncol(dfm_object),
+        "total terms that appeared in less than",threshold,"years.\n")
+
+    if (length(remove) > 0) {
+        # remove the low frequency terms
+        dfm_object <- dfm_object[,-remove]
+    }
+
+    #return the dfm_object
+    return(dfm_object)
+}
