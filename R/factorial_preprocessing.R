c3d23f49dea8191da7e3933f7275a641b1d220c6
Matt Denny <matthewjdenny@gmail.com>
2016-06-30 14:33:24 -0400

    option to preprocess docs in parallel

--- R/factorial_preprocessing.R
+++ R/factorial_preprocessing.R
@@ -12,12 +12,18 @@
 #' @param infrequent_term_threshold A proportion threshold at which infrequent
 #' terms are to be filtered. Defaults to 0.01 (terms that appear in less than
 #' 1 percent of documents).
+#' @param parallel Logical indicating whether factorial prerpocessing shoudl be
+#' performed in parallel. Defualts to FALSE.
+#' @param cores Defualts to 1, can be set to any number less than or equal to
+#' the number of cores on one's computer.
 #' @return A list object containing permutations of the document-term matrix.
 #' @export
 factorial_preprocessing <- function(text,
                                     use_ngrams = TRUE,
                                     filter_infrequent_terms = TRUE,
-                                    infrequent_term_threshold = 0.01){
+                                    infrequent_term_threshold = 0.01,
+                                    parallel = FALSE,
+                                    cores = 1){
     # check to see if input is a corpus object. If it is, extract the texts
     if (class(text)[1] == "corpus") {
         text <- quanteda::texts(text)
@@ -70,60 +76,76 @@ factorial_preprocessing <- function(text,
     # create a list object in which to store the different dfm's
     dfm_list <- vector(mode = "list", length = nrow(choices))
 
-    # loop over different preprocessing decisions
-    for (i in 1:nrow(choices)) {
-        cat("Currently working on combination",i,"of",nrow(choices),"\n")
-        # need a conditional for removing stopwords
-        if (choices$removeStopwords[i]) {
-            # generate dfm
-            if (choices$use_ngrams[i]) {
-                current_dfm <- quanteda::dfm(
-                    text,
-                    removePunct = choices$removePunctuation[i],
-                    removeNumbers = choices$removeNumbers[i],
-                    toLower = choices$lowercase[i],
-                    stem = choices$stem[i],
-                    ignoredFeatures = quanteda::stopwords(),
-                    ngrams = 1:3)
-            } else {
-                current_dfm <- quanteda::dfm(
-                    text,
-                    removePunct = choices$removePunctuation[i],
-                    removeNumbers = choices$removeNumbers[i],
-                    toLower = choices$lowercase[i],
-                    stem = choices$stem[i],
-                    ignoredFeatures = quanteda::stopwords())
-            }
-        } else {
-            # generate dfm
-            if (choices$use_ngrams[i]) {
-                current_dfm <- quanteda::dfm(
-                    text,
-                    removePunct = choices$removePunctuation[i],
-                    removeNumbers = choices$removeNumbers[i],
-                    toLower = choices$lowercase[i],
-                    stem = choices$stem[i],
-                    ngrams = 1:3)
-            } else {
-                current_dfm <- quanteda::dfm(
-                    text,
-                    removePunct = choices$removePunctuation[i],
-                    removeNumbers = choices$removeNumbers[i],
-                    toLower = choices$lowercase[i],
-                    stem = choices$stem[i])
-            }
-        }
+    if (parallel) {
+        cat("Preprocessing documents",nrow(choices),"different ways on",
+            cores,"cores. This may take a while...\n")
+        cl <- parallel::makeCluster(getOption("cl.cores", cores))
 
-        # remove infrequent terms
-        if (choices$infrequent_terms[i]) {
-            current_dfm <- remove_infrequent_terms(
-                dfm_object = current_dfm,
-                proportion_threshold = infrequent_term_threshold)
-        }
+        dfm_list <- parallel::clusterApplyLB(
+            cl = cl,
+            x = 1:nrow(choices),
+            fun = parallel_preprocess,
+            text = text,
+            infrequent_term_threshold = infrequent_term_threshold)
+        # stop the cluster when we are done
+        parallel::stopCluster(cl)
+    } else {
+        # loop over different preprocessing decisions
+        for (i in 1:nrow(choices)) {
+            cat("Currently working on combination",i,"of",nrow(choices),"\n")
+            # need a conditional for removing stopwords
+            if (choices$removeStopwords[i]) {
+                # generate dfm
+                if (choices$use_ngrams[i]) {
+                    current_dfm <- quanteda::dfm(
+                        text,
+                        removePunct = choices$removePunctuation[i],
+                        removeNumbers = choices$removeNumbers[i],
+                        toLower = choices$lowercase[i],
+                        stem = choices$stem[i],
+                        ignoredFeatures = quanteda::stopwords(),
+                        ngrams = 1:3)
+                } else {
+                    current_dfm <- quanteda::dfm(
+                        text,
+                        removePunct = choices$removePunctuation[i],
+                        removeNumbers = choices$removeNumbers[i],
+                        toLower = choices$lowercase[i],
+                        stem = choices$stem[i],
+                        ignoredFeatures = quanteda::stopwords())
+                }
+            } else {
+                # generate dfm
+                if (choices$use_ngrams[i]) {
+                    current_dfm <- quanteda::dfm(
+                        text,
+                        removePunct = choices$removePunctuation[i],
+                        removeNumbers = choices$removeNumbers[i],
+                        toLower = choices$lowercase[i],
+                        stem = choices$stem[i],
+                        ngrams = 1:3)
+                } else {
+                    current_dfm <- quanteda::dfm(
+                        text,
+                        removePunct = choices$removePunctuation[i],
+                        removeNumbers = choices$removeNumbers[i],
+                        toLower = choices$lowercase[i],
+                        stem = choices$stem[i])
+                }
+            }
 
-        # store the current dfm
-        dfm_list[[i]] <- current_dfm
+            # remove infrequent terms
+            if (choices$infrequent_terms[i]) {
+                current_dfm <- remove_infrequent_terms(
+                    dfm_object = current_dfm,
+                    proportion_threshold = infrequent_term_threshold)
+            }
+
+            # store the current dfm
+            dfm_list[[i]] <- current_dfm
+        }
     }
+
 
     # combine metadata and dfm list and return
     return_list <- list(choices = choices,
