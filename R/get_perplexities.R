fc83d7969123640b20c30dee9f10cfec5c1d5ec9
Matt Denny <matthewjdenny@gmail.com>
2016-07-22 01:20:22 -0400

    updated to work with new data

--- R/get_perplexities.R
+++ R/get_perplexities.R
@@ -1,16 +1,16 @@
 get_perplexities <- function(topics,
-                             cur_dfm){
-
-    # round all num topic values to whole numbers (allows for function to pass in
-    # multiple num topic's)
-    topics <- round(topics)
+                             cur_dfm,
+                             test_dfm){
 
     # now calculate perplexities
     perplexities <- rep(0,length(topics))
     for (i in 1:length(topics)){
+        cat("fitting model with",topics[i],"topics...\n")
         fit <- topicmodels::LDA(quanteda::convert(cur_dfm, to = "topicmodels"),
                                 k = topics[i])
-        perplexities[i] <- topicmodels::perplexity(fit)
+        perplexities[i] <- topicmodels::perplexity(
+            object = fit,
+            newdata = quanteda::convert(test_dfm, to = "topicmodels"))
     }
     return(perplexities)
 }
