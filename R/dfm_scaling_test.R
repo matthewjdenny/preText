0458b6d386af438c4877357944db8b3a2e80be97
Matt Denny <matthewjdenny@gmail.com>
2016-08-01 02:02:53 -0400

    new dam scaling test to look for outliers. Makes a pretty plot.

---
+++ R/dfm_scaling_test.R
@@ -0,0 +1,170 @@
+#' @title comparison of dfms using scaling, with a test for difference from the
+#' mean dfm.
+#' @description Scale each dfm into a 2-d space and test for outliers.
+#'
+#' @param scaling_results A list object produced by the `scaling_comparison()`
+#' function.
+#' @param labels A character vector with labels for each dfm, based on
+#' preprocessing choices.
+#' @param dimensions THe number of dimensions to be used by the multidimensional
+#' scaling algorithm. Defualts to 2.
+#' @param similarity_method The method that should be used for calculating
+#' document similarities. Defualts to "cosine".
+#' @param method Should the raw distances or scaled document positions be used
+#' for scaling? can be one of c("distances","positions"), defaults to
+#' "distances".
+#' @param return_positions Logical indicating whether dfm positions should be
+#' returned as a data.frame. Defaults to FALSE
+#' @return A result list object or a plot, or both.
+#' @export
+dfm_scaling_test <- function(scaling_results,
+                             labels,
+                             dimensions = 2,
+                             similarity_method = "cosine",
+                             method = c("distances","positions"),
+                             return_positions = FALSE){
+
+    method <- method[1]
+
+    if (method == "distances") {
+        # extract similarity matrix list
+        sim_mat_list <- scaling_results$similarity_matrices
+
+        # get the number of dfms
+        num_dfms <- length(sim_mat_list)
+
+        ncols <- (length(lower.tri(sim_mat_list[[1]])) -
+                      nrow(sim_mat_list[[1]]))/2
+        # ceate data structures to store information
+        document_distances <- matrix(0,
+                                     ncol = ncols,
+                                     nrow = num_dfms)
+
+        # populate the distance comparison matrix
+        for (i in 1:num_dfms) {
+            document_distances[i,] <- sim_mat_list[[i]][
+                lower.tri(sim_mat_list[[i]])]
+        }
+
+        # give descriptive row names
+        rownames(document_distances) <- labels
+
+        document_distances <- apply(document_distances,2,rev)
+        # now generate a distance matrix on this dataset
+        dfm_similarity <- proxy::simil(document_distances,
+                                       method = similarity_method,
+                                       diag = FALSE,
+                                       by_rows = TRUE)
+        dfm_similarity <- as.matrix(dfm_similarity)
+
+        # now scale this distance
+        pos <- stats::cmdscale(dfm_similarity, k = dimensions)
+        rownames(pos)[1] <- " "
+    } else {
+        # now do the same thing but on the procrustes transformed scaled document
+        # positions
+        positions_list <- scaling_results$scaled_positions
+        anchor_positions <- positions_list[[1]]
+        anchor_positions <- anchor_positions[order(rownames(anchor_positions)),]
+
+        document_positions <- matrix(0,
+                                     ncol = length(c(anchor_positions)),
+                                     nrow = num_dfms)
+
+        document_positions[1,] <- c(anchor_positions)
+        # rotate the other positions and put them together in a big data.frame
+        for (i in 2:num_dfms) {
+            cur_pos <- positions_list[[i]][order(rownames(positions_list[[i]])),]
+            cur <- vegan::procrustes(anchor_positions,
+                                     cur_pos,
+                                     scale = F)$Yrot
+
+            document_positions[i,] <- c(cur)
+        }
+
+        # set rownames before reversing
+        rownames(document_positions) <- labels
+
+        # reverse order to have no preprocessing first
+        document_positions <- apply(document_positions,2,rev)
+
+        # now generate a distance matrix on this dataset
+        dfm_similarity2 <- proxy::simil(document_positions,
+                                        method = similarity_method,
+                                        diag = FALSE,
+                                        by_rows = TRUE)
+        dfm_similarity2 <- as.matrix(dfm_similarity2)
+
+        # now scale this distance
+        pos <- stats::cmdscale(dfm_similarity2, k = dimensions)
+        rownames(pos)[1] <- " "
+    }
+
+
+    # now we calculate mean, distance from mean, distribution of distances from
+    # mean, t-test on distances, and draw circle in plot.
+
+    mean_values <- rep(0,dimensions)
+    for(i in 1:dimensions) {
+        mean_values[i] <- mean(pos[,i])
+    }
+
+    # now calculate euclidean distances from mean value
+    euclidean_distance_from_mean <- rep(0, num_dfms)
+    for (i in 1:num_dfms) {
+       dist <- 0
+       for (j in 1:dimensions) {
+           dist <- dist + (pos[i,j] - mean_values[j])^2
+       }
+       euclidean_distance_from_mean[i] <- sqrt(dist)
+    }
+
+    distance_sd <- sd(euclidean_distance_from_mean)
+
+    significance_threshold <- 1.645 * distance_sd
+
+    remove <- which(euclidean_distance_from_mean < significance_threshold)
+
+    UMASS_BLUE <- rgb(51,51,153,195,maxColorValue = 255)
+    UMASS_RED <- rgb(153,0,51,195,maxColorValue = 255)
+
+    plot(pos[,1:2],
+         pch = 19,
+         col = UMASS_BLUE,
+         xlab = "Dimension 1",
+         ylab = "Dimension 2",
+         main = "Document-Term Matrix Scaled Positions")
+
+    # now draw circle for significance threshold
+    r <- significance_threshold
+    nseg <- 360
+    x.cent <- mean_values[1]
+    y.cent <- mean_values[2]
+    xx <- x.cent + r*cos( seq(0,2*pi, length.out=nseg) )
+    yy <- y.cent + r*sin( seq(0,2*pi, length.out=nseg) )
+    lines(xx, yy, col=UMASS_RED)
+
+    # now add in names for points outside of significance threshold
+    to_plot <- rownames(pos)
+    if (length(remove) > 0) {
+        to_plot[remove] <- " "
+    }
+    text(pos[,1:2],to_plot)
+
+    # create dataframe to return
+    different_from_mean <- rep(TRUE,num_dfms)
+    if (length(remove) > 0) {
+        different_from_mean[remove] <- FALSE
+    }
+    dfm_positions <- pos
+    dfm_positions <- as.data.frame(dfm_positions)
+    colnames(dfm_positions) <- paste("Dim_",1:ncol(dfm_positions),sep = "")
+    dfm_positions <- cbind(dfm_positions,
+                           euclidean_distance_from_mean,
+                           different_from_mean)
+
+    if (return_positions) {
+        return(dfm_positions)
+    }
+}
+
