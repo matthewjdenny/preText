66136a4bd5470098363bd3c4701916e4d18c475c
Matt Denny <matthewjdenny@gmail.com>
2016-08-12 09:21:37 -0400

    make everything distances

--- R/scaling_comparison.R
+++ R/scaling_comparison.R
@@ -5,19 +5,20 @@
 #' @param dfm_object_list A list of quanteda dfm() objects.
 #' @param dimensions THe number of dimensions to be used by the multidimensional
 #' scaling algorithm. Defualts to 2.
-#' @param similarity_method The method that should be used for calculating
-#' document similarities. Defualts to "cosine".
+#' @param distance_method The method that should be used for calculating
+#' document distances. Defualts to "cosine".
 #' @return A result list object.
 #' @export
 scaling_comparison <- function(dfm_object_list,
                                dimensions = 2,
-                               similarity_method = "cosine"){
+                               distance_method = "cosine"){
 
     # get the number of dfms
     num_dfms <- length(dfm_object_list)
 
     # ceate data structures to store information
-    similarity_matrices <- vector(mode = "list", length = num_dfms)
+    distance_matrices <- vector(mode = "list", length = num_dfms)
+    distance_objects <- vector(mode = "list", length = num_dfms)
     scaled_positions <- vector(mode = "list", length = num_dfms)
 
     for (i in 1:num_dfms) {
@@ -27,22 +28,26 @@ scaling_comparison <- function(dfm_object_list,
         cur_dfm <- dfm_object_list[[i]]
 
         # calculate the document similarity matrix
-        simil <- quanteda::similarity(cur_dfm, method = similarity_method)
-        sim <- as.matrix(simil)
+        simil <- quanteda::similarity(cur_dfm, method = distance_method)
+        simil <- as.matrix(simil)
+        distances2 <- proxy::pr_simil2dist(simil)
+        distances <- as.matrix(distances2)
 
         # get positions for each document in 2d space
-        pos <- stats::cmdscale(sim, k = dimensions)
+        pos <- stats::cmdscale(distances, k = dimensions)
 
         # store everything
-        similarity_matrices[[i]] <- sim
+        distance_matrices[[i]] <- distances
+        distance_objects[[i]] <- proxy::as.dist(distances2)
         scaled_positions[[i]] <- pos
 
         t2 <- proc.time() - ptm
         cat("Complete in:",t2[[3]],"seconds...\n")
     }
 
-    return(list(similarity_matrices = similarity_matrices,
-                scaled_positions = scaled_positions))
+    return(list(distance_matrices = distance_matrices,
+                scaled_positions = scaled_positions,
+                dist_objects = distance_objects))
 
 }
 
