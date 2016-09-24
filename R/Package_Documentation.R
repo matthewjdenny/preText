6583d5ff118d047f6c19c774aedbad645e75390c
Matt Denny <matthewjdenny@gmail.com>
2016-09-24 13:49:22 -0400

    change name to preText

--- R/Package_Documentation.R
+++ R/Package_Documentation.R
@@ -1,10 +1,10 @@
-#' preptest: fucntions to asses the effects of text preprocessing decisions
+#' preText: fucntions to asses the effects of text preprocessing decisions
 #'
-#' @section preptest functions:
+#' @section preText functions:
 #' To use this package, You will first want to check out the factorial_preprocessing() function which will take raw data and transform it into document-frequency matrices using a factorial desig and five different preprocessing decisions.
 #'
 #' @docType package
-#' @name preptest
+#' @name preText
 NULL
 #> NULL
 
