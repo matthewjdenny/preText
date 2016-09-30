10a948e84617db0d2738a3de90996a5970c81d23
Matt Denny <matthewjdenny@gmail.com>
2016-09-29 21:36:20 -0400

    give a more general overview of how the package works

--- R/Package_Documentation.R
+++ R/Package_Documentation.R
@@ -1,7 +1,15 @@
-#' preText: fucntions to asses the effects of text preprocessing decisions
+#' preText: functions to asses the effects of text preprocessing decisions
 #'
 #' @section preText functions:
-#' To use this package, You will first want to check out the factorial_preprocessing() function which will take raw data and transform it into document-frequency matrices using a factorial desig and five different preprocessing decisions.
+#' To use this package, You will first want to check out the
+#' factorial_preprocessing() function which will take raw data and transform it
+#' into document-frequency matrices using a factorial design and 6-7 different
+#' preprocessing decisions. The next step in most applications will be to run
+#' the preText() function, which will generate preText scores for each preprocessing
+#' specification. These can then be fed to the preText_score_plot() and
+#' regression_coefficient_plot() functions to generate interpretable output.
+#' For more information on additional functions check out the GitHub READMe for
+#' this package (https://github.com/matthewjdenny/preText).
 #'
 #' @docType package
 #' @name preText
