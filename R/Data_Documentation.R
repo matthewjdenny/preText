fd989ae2f5986c811d561031cc441da325389750
Matt Denny <matthewjdenny@gmail.com>
2016-10-01 20:28:21 -0400

    added UK manifestos data

---
+++ R/Data_Documentation.R
@@ -0,0 +1,9 @@
+#' Full text of 69 UK party manifestos from 1918-2001.
+#'
+#' A dataset of party manifestos for Liberal, Labour, and Conservative parties
+#' for every general election in the UK between 1918 and 2001 (23 elections).
+#'
+#' @format A character vector of length 69, with one document per entry.
+#' @source Aggregated from several online resources but see, for example:
+#' \url{http://www.politicsresources.net/area/uk/man.htm}
+"UK_Manifestos"
