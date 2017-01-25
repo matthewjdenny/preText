cbb89e8699ab257b6aae43501fa7d1597f767aee
Matt Denny <matthewjdenny@gmail.com>
2017-01-25 11:37:52 -0500

    switch to inaugural texts new name

--- tests/testthat/test_factorial_preprocessing.R
+++ tests/testthat/test_factorial_preprocessing.R
@@ -1,7 +1,7 @@
 context("Factorial Preprocessing")
 test_that("Small example works", {
     # load data
-    corp <- quanteda::corpus(quanteda::inaugTexts)
+    corp <- quanteda::corpus(quanteda::data_char_inaugural)
     corp <- quanteda::texts(corp)[1:3]
 
     # run preprocessor
