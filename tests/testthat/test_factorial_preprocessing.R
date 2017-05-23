e5391fab1ea947e3cf3600484a30543693475487
Matt Denny <matthewjdenny@gmail.com>
2017-05-23 00:35:30 -0400

    test now uses correct call to import

--- tests/testthat/test_factorial_preprocessing.R
+++ tests/testthat/test_factorial_preprocessing.R
@@ -1,8 +1,9 @@
 context("Factorial Preprocessing")
 test_that("Small example works", {
     # load data
-    corp <- quanteda::corpus(quanteda::data_char_inaugural)
-    corp <- quanteda::texts(corp)[1:3]
+    documents <- quanteda::data_corpus_inaugural
+    # use first 3 documents for example
+    corp <- documents[1:3,]
 
     # run preprocessor
     factorial_prep <- factorial_preprocessing(corp)
