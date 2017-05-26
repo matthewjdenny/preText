bd287c1fb813a008b7e634fb4a3090abf68da752
Matt Denny <matthewjdenny@gmail.com>
2017-05-25 22:15:05 -0400

    use 10 documents but no ngrams

--- tests/testthat/test_factorial_preprocessing.R
+++ tests/testthat/test_factorial_preprocessing.R
@@ -3,10 +3,11 @@ test_that("Small example works", {
     # load data
     documents <- quanteda::data_corpus_inaugural
     # use first 3 documents for example
-    corp <- documents[1:3,]
+    corp <- documents[1:10,]
 
     # run preprocessor
-    factorial_prep <- factorial_preprocessing(corp)
+    factorial_prep <- factorial_preprocessing(corp,
+                                              use_ngrams = FALSE)
 
 
 })
