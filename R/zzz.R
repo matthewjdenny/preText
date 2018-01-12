486a2722d6828fa3fe5b83cc05cddb397b516aa1
Matt Denny <matthewjdenny@gmail.com>
2018-01-12 12:02:13 -0500

    version bump and date bump

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage("preText: Diagnostics to Assess the Effects of Text Preprocessing Decisions
-Version 0.6.1 created on 2017-11-13.
+Version 0.6.3 created on 2018-01-12.
 copyright (c) 2017, Matthew J. Denny, Penn State University
                     Arthur Spirling, NYU
 Type vignette('getting_started_with_preText') to get started.
