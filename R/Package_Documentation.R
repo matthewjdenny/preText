35feaa97d0ef4e96d151228d9687f9d993998bc8
Matt Denny <matthewjdenny@gmail.com>
2021-07-25 15:08:30 -0400

    pacakge version bump with new import and fixes

--- R/Package_Documentation.R
+++ R/Package_Documentation.R
@@ -33,3 +33,7 @@ NULL
 #' @import quanteda.textmodels
 NULL
 
+#' @import quanteda.textstats
+NULL
+
+
--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage("preText: Diagnostics to Assess the Effects of Text Preprocessing Decisions
-Version 0.7.1 created on 2020-06-06.
-copyright (c) 2020, Matthew J. Denny, Georgetown University
+Version 0.7.2 created on 2021-07-25.
+copyright (c) 2021, Matthew J. Denny, Georgetown University
                     Arthur Spirling, NYU
 Type vignette('getting_started_with_preText') to get started.
 Development website: https://github.com/matthewjdenny/preText")
